37be48461f72fdcdf2b1caa1625766349dac69b4
aryan68125 <aryan68125@gmail.com>
2025-06-03 09:19:22 +0530

    email related settings and installed django-celery library and updated the requirements.txt file

--- chat/settings.py
+++ chat/settings.py
@@ -158,3 +158,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# EMAIL REALTED SETTINGS 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
