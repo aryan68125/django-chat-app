2d7595678bce2ebe80c9f57d4858c10ae9c2833e
aryan68125 <aryan68125@gmail.com>
2025-06-04 09:39:31 +0530

    Successfully integrated email send feature in the application

--- auth_app/tasks.py
+++ auth_app/tasks.py
@@ -1,7 +1,25 @@
+from __future__ import absolute_import, unicode_literals
 from celery import Celery
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.core.mail import EmailMultiAlternatives
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
-@app.task
-def add(x, y):
-    return x + y
+def send_email(subject,message,EMAIL_HOST_USER,recipient_list,html_message=None):
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=message,
+        from_email=EMAIL_HOST_USER,
+        to=recipient_list
+    )
+    if html_message:
+        email.attach_alternative(html_message,"text/html")
+    return email.send(fail_silently=False)
+
+logger = get_task_logger(__name__)
+
+@shared_task(name="send_email_task")
+def send_email_task(subject, message, EMAIL_HOST_USER, recipient_list, html_message = None):
+    logger.info("Mail Sent!")
+    return send_email(subject, message, EMAIL_HOST_USER, recipient_list, html_message)
--- auth_app/views_bl.py
+++ auth_app/views_bl.py
@@ -6,26 +6,30 @@ from rest_framework import status
 # import response utility
 from common_app.views_bl import CommonResponse
 
-# just for testing purposes 
-from auth_app.tasks import add
+# celery related imports
+from auth_app.tasks import send_email_task
 from celery.result import AsyncResult
 from django_celery_results.models import TaskResult
 
+#email related imports
+from django.template.loader import render_to_string
+from django.conf import settings
+
 class RegisterUser(APIView, CommonResponse):
     def post(self, request):
-        x = int(request.data.get('x', 0))
-        y = int(request.data.get('y', 0))
-        result = add.delay(x, y)
-        task_result = AsyncResult(result.id)
-
-        data = {
-            "task_id": result.id,
-            "status": task_result.status,
-            "result": task_result.result if task_result.ready() else None,
-        }
+        subject = "This is a test email sent by django-chat-app"
+        message = "Hello, This is a test email sent by django-chat-app"
+        if not subject:
+            return self.common_web_response(status_code=status.HTTP_404_NOT_FOUND,error="Email subject not found!")
+        if not message:
+            return self.common_web_response(status_code=status.HTTP_404_NOT_FOUND,error="Email message body not found!")
+        html_message = render_to_string('email_templates/register_verify_email.html',{
+            "user_name":"Aditya Kumar test user from views"
+        })
+        recipient_list = ["aryan68125@gmail.com"]
+        send_email_task.delay(subject,message,settings.EMAIL_HOST_USER,recipient_list,html_message)
         return self.common_web_response(
             status_code=status.HTTP_201_CREATED,
-            message="Task submitted successfully.",
-            data=data
+            message="Email sent successfully.",
         )
 
