c5d0f0c71a77fd083f3d90c89a646bd03c529d41
aryan68125 <aryan68125@gmail.com>
2025-07-01 17:44:28 +0530

    Added a prototype in html code to update the profile picture

--- static/js/user_app/user_profile_page.js
+++ static/js/user_app/user_profile_page.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    console.log("User Profile js ready!")
+    add_or_update_profile_picture_data_in_profile_page_handler()
 })
+
+// ADD OR UPDATE PROFILE PICTURE DATA IN THE FORM STARTS
+function add_or_update_profile_picture_data_in_profile_page_handler(){
+    $(document).on("click","#select_profile_photo",function(){
+        console.log("Select profile photo button clicked!")
+        $("#profile_photo_input").click()
+    })
+
+    // Preview selected image
+    $("#profile_photo_input").on("change",function(){
+        const file = this.files[0]
+        if(file){
+            const reader = new FileReader()
+            reader.onload = function(e){
+                $("#profile_photo_preview").attr("src",e.target.result)
+            }
+            reader.readAsDataURL(file)
+        }
+    })
+}
+// ADD OR UPDATE PROFILE PICTURE DATA IN THE FORM ENDS
